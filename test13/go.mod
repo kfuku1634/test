03610fa4fb9ef9a0deb49f9f7737a1614c0c670e
kfuku1634 <kfuku1634.dev@gmail.com>
2022-10-13 22:08:44 +0900

    go test

---
+++ test13/go.mod
@@ -0,0 +1,5 @@
+module m
+
+go 1.18
+
+require github.com/kfuku1634/hogemodule v0.0.6 // indirect
---
+++ test13/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+    "github.com/kfuku1634/hogemodule"
+    "github.com/kfuku1634/hogemodule/hogedir"
+    "fmt"
+)
+
+func main() {
+    fmt.Println(hogemodule.Hoge())
+    fmt.Println(hogedir.Hogedir())
+}
